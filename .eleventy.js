3fd49c8cee6ebc5afde0b95ff37511191b9d139f
Jens Andreasson <jensandreasson77@gmail.com>
2024-05-27 20:01:53 +0200

    code highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const markdownIt = require("markdown-it")
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/style.css")
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  
+  eleventyConfig.addPassthroughCopy("./src/css/")
+  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+  eleventyConfig.addPlugin(syntaxHighlight)
+
   let options = {
     html: true,
     breaks: true,
@@ -16,7 +18,7 @@ module.exports = function (eleventyConfig) {
     markdownTemplateEngine: "njk",
     dir: {
       input: "src",
-      output: "dist"
+      output: "dist",
     },
     passthroughFileCopy: true,
   }
